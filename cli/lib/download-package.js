cd3fadf54357202d1c28cd9b4a23b4b0e76a0b93
babblebey <babblebey@gmail.com>
2024-03-10 22:53:05 +0100

    chore: hold no usage of env var

--- cli/lib/download-package.js
+++ cli/lib/download-package.js
@@ -1,10 +1,10 @@
-import { cleanEnv } from "envalid";
+// import { cleanEnv } from "envalid";
 import { downloadTemplate } from "giget";
-import { isNotEmptyStr } from "../utils/env-validators.js";
+// import { isNotEmptyStr } from "../utils/env-validators.js";
 
-const env = cleanEnv(process.env ,{
-  REPO_FULLNAME: isNotEmptyStr()
-});
+// const env = cleanEnv(process.env ,{
+//   REPO_FULLNAME: isNotEmptyStr()
+// });
 
 /**
  * Download Package from the cwpt repository's `package` directory 
@@ -13,9 +13,9 @@ const env = cleanEnv(process.env ,{
  * @returns {Promise<{dir: string, source: string}>}
  */
 export default async function downloadCWPTPackage(packageSubdir, targetDir) {
-  const { dir, source } = await downloadTemplate(`github:${env.REPO_FULLNAME}/packages/${packageSubdir}#develop`, {
+  const { dir, source } = await downloadTemplate(`github:babblebey/create-wp-theme/packages/${packageSubdir}#develop`, {
     dir: targetDir,
-    repo: env.REPO_FULLNAME,
+    repo: "babblebey/create-wp-theme",
     force: true,
     cwd: "."
   });
