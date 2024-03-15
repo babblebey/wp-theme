d60df48fb35f08b4becdf9553e574dc91f9c57d9
Olabode Lawal-Shittabey <babblebey@gmail.com>
2024-03-15 13:25:10 +0100

    chore: nit for first release (#16)
    * fix: remove `envalid`
    
    * chore: change package down ref to `main`
    
    * feat: implement `trycatch` in `downloadCWPTPackage`
    
    * fix: add missing `cancel` import

--- cli/lib/download-package.js
+++ cli/lib/download-package.js
@@ -1,10 +1,5 @@
-// import { cleanEnv } from "envalid";
+import { cancel } from "@clack/prompts";
 import { downloadTemplate } from "giget";
-// import { isNotEmptyStr } from "../utils/env-validators.js";
-
-// const env = cleanEnv(process.env ,{
-//   REPO_FULLNAME: isNotEmptyStr()
-// });
 
 /**
  * Download Package from the cwpt repository's `package` directory 
@@ -13,12 +8,22 @@ import { downloadTemplate } from "giget";
  * @returns {Promise<{dir: string, source: string}>}
  */
 export default async function downloadCWPTPackage(packageSubdir, targetDir) {
-  const { dir, source } = await downloadTemplate(`github:babblebey/create-wp-theme/packages/${packageSubdir}#develop`, {
-    dir: targetDir,
-    repo: "babblebey/create-wp-theme",
-    force: true,
-    cwd: "."
-  });
-
-  return { dir, source };
+  try {
+    const { dir, source } = await downloadTemplate(`github:babblebey/create-wp-theme/packages/${packageSubdir}#main`, {
+      dir: targetDir,
+      repo: "babblebey/create-wp-theme",
+      force: true,
+      cwd: "."
+    });
+  
+    return { dir, source };
+  } catch (error) {
+    if (error instanceof Error) {
+      cancel(error.message);
+      process.exit(1);
+    } else {
+      cancel("Unable to download package");
+      process.exit(1);
+    }
+  }
 }
