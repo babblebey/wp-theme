e3e93263a566d90ab68d5d9837cd853be70842cd
babblebey <babblebey@gmail.com>
2024-03-15 11:38:39 +0100

    feat: integrate `resolveCommand` into `run` script

--- cli/index.js
+++ cli/index.js
@@ -6,6 +6,7 @@ import color from "picocolors";
 import minimist from "minimist";
 import { intro, cancel, spinner, note } from "@clack/prompts";
 import { fileExists } from "./utils/file-exist.js";
+import resolveCommand from "./lib/resolve-command.js";
 import resolveThemeName from "./lib/resolve-theme-name.js";
 import resolveCSSFramework from "./lib/resolve-css-framework.js";
 import injectThemeName from "./lib/inject-theme-name.js";
@@ -17,71 +18,76 @@ async function run() {
   const cwd = process.cwd();
   
   // start
-  intro(color.inverse("create-wp-theme"));
+  intro(color.inverse("wp-theme"));
 
   // get arguments
   const argv = minimist(process.argv.slice(2));
 
-  // get theme name from argv
-  // const themeNameFromArgv = argv._[0];
-  const themeName = await resolveThemeName(argv._[0]);
+  // get command and theme name from argv
+  const commandFromArgv = argv._[0];
+  const themeNameFromArgv = argv._[1];
 
-  // instantiate spinner
-  const s = spinner();
+  const command = await resolveCommand(commandFromArgv);
+  const themeName = await resolveThemeName(themeNameFromArgv);
 
-  // compute location to save file to
-  const filePath = path.join(cwd, themeName);
-
-  // check if location already exist
-  if (fileExists(filePath)) {
-    console.log();
-    cancel(color.red(`${filePath} already exists.`));
-    process.exit(1);
+  if (command === "new") {
+    // instantiate spinner
+    const s = spinner();
+  
+    // compute location to save file to
+    const filePath = path.join(cwd, themeName);
+  
+    // check if location already exist
+    if (fileExists(filePath)) {
+      console.log();
+      cancel(color.red(`${filePath} already exists.`));
+      process.exit(1);
+    }
+  
+    // get specified css framework - only if supported
+    const cssFramework = await resolveCSSFramework(argv);
+  
+    s.start("Copying theme files");
+  
+    // download cssFramework related package
+    const templateToCopy = `templates/cwpt-${cssFramework}`;
+    await downloadCWPTPackage(templateToCopy, themeName);
+  
+    // download node_script package
+    await downloadCWPTPackage("node_scripts", `${themeName}/node_scripts`);
+  
+    // download base `theme` package
+    await downloadCWPTPackage("theme", `${themeName}/theme`);
+  
+    s.stop("Files copied");
+  
+    s.start("Setting things up");
+  
+    // modify placeholders in package.json `bundle` script
+    const packageJsonFile = path.resolve(filePath, "package.json");
+    const packageJsonContent = fs.readFileSync(packageJsonFile, "utf-8");
+    injectThemeName(packageJsonFile, packageJsonContent, themeName);
+  
+    // modify placeholders in `theme/style.css`
+    const styleCssFile = path.resolve(filePath, "theme/style.css");
+    const styleCssContent = fs.readFileSync(styleCssFile, "utf-8");
+    injectThemeName(styleCssFile, styleCssContent, themeName);
+    injectCSSFramework(styleCssFile, styleCssContent, cssFramework);
+  
+    /**
+     * @todo consider modifying namespaces `cwpt` in `function.php` to theme name
+     */
+  
+    s.stop("You're all set!");
+  
+    // detect package manager and its command prefix
+    const { packageManager, commandPrefix } = resolvePackageManager();
+  
+    note(
+      `${color.cyan(`cd ${path.relative(cwd, filePath)}`)}\n${color.cyan(`${packageManager} install`)}\n${color.cyan(`${commandPrefix} watch`)}`, 
+      "Now run"
+    );
   }
-
-  // get specified css framework - only if supported
-  const cssFramework = await resolveCSSFramework(argv);
-
-  s.start("Copying theme files");
-
-  // download cssFramework related package
-  const templateToCopy = `templates/cwpt-${cssFramework}`;
-  await downloadCWPTPackage(templateToCopy, themeName);
-
-  // download node_script package
-  await downloadCWPTPackage("node_scripts", `${themeName}/node_scripts`);
-
-  // download base `theme` package
-  await downloadCWPTPackage("theme", `${themeName}/theme`);
-
-  s.stop("Files copied");
-
-  s.start("Setting things up");
-
-  // modify placeholders in package.json `bundle` script
-  const packageJsonFile = path.resolve(filePath, "package.json");
-  const packageJsonContent = fs.readFileSync(packageJsonFile, "utf-8");
-  injectThemeName(packageJsonFile, packageJsonContent, themeName);
-
-  // modify placeholders in `theme/style.css`
-  const styleCssFile = path.resolve(filePath, "theme/style.css");
-  const styleCssContent = fs.readFileSync(styleCssFile, "utf-8");
-  injectThemeName(styleCssFile, styleCssContent, themeName);
-  injectCSSFramework(styleCssFile, styleCssContent, cssFramework);
-
-  /**
-   * @todo consider modifying namespaces `cwpt` in `function.php` to theme name
-   */
-
-  s.stop("You're all set!");
-
-  // detect package manager and its command prefix
-  const { packageManager, commandPrefix } = resolvePackageManager();
-
-  note(
-    `${color.cyan(`cd ${path.relative(cwd, filePath)}`)}\n${color.cyan(`${packageManager} install`)}\n${color.cyan(`${commandPrefix} watch`)}`, 
-    "Now run"
-  );
 }
 
 run().catch(console.error);
